f1bcefa94a3dd9857665bd502e9cc1db00af5ff4
Jake Lipson <jake_lipson@yahoo.com>
2024-07-06 00:54:40 -0400

    Lesson 4.8: Jump Pad

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Player
 
 @export var gravity = 400
 @export var jump_force = 200
@@ -13,7 +14,8 @@ func _physics_process(delta):
 			velocity.y = 500
 	
 	if Input.is_action_just_pressed("jump") && is_on_floor():
-		velocity.y = -jump_force
+		print(jump_force)
+		jump(jump_force)
 	
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction != 0:
@@ -23,6 +25,9 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	update_animations(direction)
+
+func jump(force):
+	velocity.y = -force
 	
 func update_animations(direction):
 	if is_on_floor():
