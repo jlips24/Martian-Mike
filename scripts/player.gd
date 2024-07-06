17710a8fd1813d52283320bc6c2d9347869ddfb4
Jake Lipson <jake_lipson@yahoo.com>
2024-07-06 00:16:02 -0400

    Lesson 4.4: Player Movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,20 @@
 extends CharacterBody2D
 
+@export var gravity = 400
+@export var jump_force = 200
+@export var speed = 125
+
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("move_right"):
-		animated_sprite.play("run")
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		if velocity.y > 500:
+			velocity.y = 500
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+	move_and_slide()
