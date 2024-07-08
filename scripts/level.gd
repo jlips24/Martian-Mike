0e10b0ddb9e45e646b071f05c098b57b57a655e0
Jake Lipson <jake_lipson@yahoo.com>
2024-07-08 00:51:53 -0400

    Lesson 4.18: Changing Scenes

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+@export var next_level: PackedScene = null
+
 @onready var start = $Start
+@onready var exit = $Exit
 
 var player = null
 
@@ -12,6 +15,7 @@ func _ready():
 	for trap in traps:
 		#trap.connect("touched_player", _on_trap_touched_player)
 		trap.touched_player.connect(_on_trap_touched_player)
+	exit.body_entered.connect(_on_exit_body_entered)
 
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
@@ -29,3 +33,10 @@ func _on_trap_touched_player():
 func reset_player():
 	player.velocity = Vector2.ZERO
 	player.global_position = start.get_spawn_pos()
+
+func _on_exit_body_entered(body):
+	if body is Player && next_level:
+		exit.animate()
+		player.active = false
+		await get_tree().create_timer(1.5).timeout
+		get_tree().change_scene_to_packed(next_level)
