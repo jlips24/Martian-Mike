a9c0a4101f9cb1c38b1aec16253e98ba4ed9f2e5
Jake Lipson <jake_lipson@yahoo.com>
2024-07-09 01:09:14 -0400

    Lesson 4.24: Level Design

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/level.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
